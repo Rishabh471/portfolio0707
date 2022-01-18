999a8ded162e2e87407545ed02e67b33afe40400
Rishabh471 <64025471+Rishabh471@users.noreply.github.com>
2022-01-18 13:55:54 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,10 +10,10 @@ const Hero = (props) => (
       <LeftSection>
         <SectionTitle main center>
           Welcome To <br />
-          My Personal Portfolio
+          My Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+         I am begignner developer aspire to learn trending Technologies.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
