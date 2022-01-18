65579793f9955472a0f09ca18aac6f9821be7aaf
Rishabh471 <64025471+Rishabh471@users.noreply.github.com>
2022-01-18 16:57:50 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,8 @@ export const projects = [
     description: "This is a real time chat application build using chatengine.io API",
       image: '/images/4.jpg',
       tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://github.com/Rishabh471/vaarta4',
-    visit: 'https://pedantic-hoover-4064f6.netlify.app/',
+    source: 'https://pedantic-hoover-4064f6.netlify.app/',
+    visit: 'https://github.com/Rishabh471/vaarta4',
     id: 0,
   },
   {
@@ -13,23 +13,23 @@ export const projects = [
     description:"This is simple drum kit site where you can use your keyboard key to have some fun at your workplace.",
     image: '/images/2.png',
     tags: ['HTML5/CSS3', 'JavaScript'],
-    source: 'https://github.com/Rishabh471/drumkit',
-    visit: 'https://quirky-meitner-e5fd9d.netlify.app/',
+    source: 'https://quirky-meitner-e5fd9d.netlify.app/',
+    visit: 'https://github.com/Rishabh471/drumkit',
     id: 1,
   },
   {
     title: 'Advice App',
     description: "This is a simple advice app which is using radom advice generator API ",
       image: '/images/3.png',
-      tags: ['HTML5/CSS3', 'JavaScript','adviceslip'],
-    source: 'https://github.com/Rishabh471/adviceme',
-    visit: 'https://romantic-wing-eee967.netlify.app/',
+      tags: ['HTML5/CSS3', 'JavaScript'],
+    source: 'https://romantic-wing-eee967.netlify.app/',
+    visit: 'https://github.com/Rishabh471/adviceme',
     id: 2,
   },
 ];
 
 export const TimeLineData = [
   { year: 2020, text: 'Worked as Subject Matter Expert of physics', },
-  { year: 2021, text: 'Till July alognside my work i learned HTML/CSS/Javascript/node.js/mongoDB/React.js',},
+  { year: 2021, text: 'Till November alognside my work i learned node.js/mongoDB/React.js',},
   { year:2022, text:"I have tried to work on project and lern new trends",},
 ];
